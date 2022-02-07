6ceb01e1c8ccae7b81bc409f4e72d7fbc9b79117
Michael McEniry <mcenirm@mceniry.net>
2022-02-07 17:02:49 -0600

    download wsusoffline

---
+++ settings.bash
@@ -0,0 +1,14 @@
+base=$(dirname -- "$BASH_SOURCE")
+downloads=$base/downloads
+
+wsusoffline_base_url=https://gitlab.com/wsusoffline/wsusoffline
+wsusoffline_release_tag=12.6.1_CommunityEdition
+wsusoffline_release_name=wsusofflineCE1261hf1
+wsusoffline_zip_gitlabdiskhash=9c12b49c6798155ee71c56e2b972d000
+wsusoffline_hashes_gitlabdiskhash=af62cb5f18760936f5396e279dd698e1
+wsusoffline_uploads_url=$wsusoffline_base_url/uploads
+wsusoffline_release_url=$wsusoffline_base_url/-/releases/$wsusoffline_release_tag
+wsusoffline_zip_file=$wsusoffline_release_name.zip
+wsusoffline_zip_url=$wsusoffline_uploads_url/$wsusoffline_zip_gitlabdiskhash/$wsusoffline_zip_file
+wsusoffline_hashes_file=${wsusoffline_release_name}_hashes.txt
+wsusoffline_hashes_url=$wsusoffline_uploads_url/$wsusoffline_hashes_gitlabdiskhash/$wsusoffline_hashes_file
---
+++ wsusoffline-download.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+. "$(dirname -- "$BASH_SOURCE")"/settings.bash
+
+mkdir -pv -- "$downloads"
+cd -- "$downloads"
+
+downloaded_once=false
+until hashdeep -s -a -l -k "$wsusoffline_hashes_file" "$wsusoffline_zip_file"
+do
+  if $downloaded_once ; then exit 1 ; fi
+  for u in "$wsusoffline_hashes_url" "$wsusoffline_zip_url"
+  do
+    curl -gsLRO "$u"
+    ls -ld -- "${u##*/}"
+  done
+  downloaded_once=true
+done
